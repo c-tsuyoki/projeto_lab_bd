80b6031479a02f34e4703ce03fc28f5e10da1eb7
c-tsuyoki <cristian175.contato@gmail.com>
2021-06-15 17:03:53 -0300

    fix fk

--- quaseTudo/tela_cadastro_login.php
+++ quaseTudo/tela_cadastro_login.php
@@ -22,20 +22,24 @@
 		<p> Login: <input type="text" size="80" name="login" required>
         <p> Senha: <input type="password" size="80" name="senha_login" required>
         <p> Tipo: <input type="text" size="80" name="tipo" required>
-		<label> Cliente: </label>
-		<select name="id_cliente"> 
-		<?php
-		  include("conexao.php");
-		  $query= 'SELECT * FROM clientes ORDER BY nome;';
-		  $resu = mysqli_query($con,$query) or die(mysqli_connect_error());
-		  while ($reg = mysqli_fetch_array($resu)){
-        ?>
-        <option value="<?php echo $reg['id'];?>"> <?php echo $reg['nome'];?> </option> 
-		<?php
-		   
-		  }
-		?>  
-		</select>
+		
+		<div>
+            <label> Cliente: </label>
+            <select required name="id_cliente">
+                <option selected disabled>Selecione o Cliente</option>
+                <?php
+                    include("conexao.php");
+                    $query = 'SELECT * FROM clientes ORDER BY id';
+                    $result = mysqli_query($con, $query) or die(mysqli_connect_error());
+                    while ($cat = mysqli_fetch_array($result)) {
+                ?>
+                <option value="<?php echo $cat['id'] ?>"><?php echo $cat['nome_cli'] ?></option>
+                <?php
+                    }
+                ?>
+            </select>
+        </div>
+
 		<p> <input type="submit" value="Enviar">
 		<p> <input type="reset" value="Limpar">
 		</form>
--- quaseTudo/tela_cadastro_orcamentos.php
+++ quaseTudo/tela_cadastro_orcamentos.php
@@ -3,6 +3,7 @@
 		session_start();
 	}	
 ?>		
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -20,53 +21,64 @@
 		<p><h1> Orçamentos - inclusão</h1></p>
 		<form method="POST" action="inclusao_orcamentos.php">
 		<p> Data: <input type="date" name="data" required>
-        <p> Valor: <input type="text" size="80" name="valor" required>
-        <label> Prestador: </label>
-		<select name="id_prestador"> 
-		<?php
-		  include("conexao.php");
-		  $query= 'SELECT * FROM prestadores ORDER BY nome;';
-		  $resu = mysqli_query($con,$query) or die(mysqli_connect_error());
-		  while ($reg = mysqli_fetch_array($resu)){
-        ?>
-        <option value="<?php echo $reg['id'];?>"> <?php echo $reg['nome'];?> </option> 
-		<?php
-		   
-		  }
-		?>  
-		</select>
-        <p> Data Expiração: <input type="date" name="data_expiracao" required>
-		<label> Cliente: </label>
-		<select name="id_cliente"> 
-		<?php
-		  include("conexao.php");
-		  $query= 'SELECT * FROM clientes ORDER BY nome;';
-		  $resu = mysqli_query($con,$query) or die(mysqli_connect_error());
-		  while ($reg = mysqli_fetch_array($resu)){
-        ?>
-        <option value="<?php echo $reg['id'];?>"> <?php echo $reg['nome'];?> </option> 
-		<?php
-		   
-		  }
-		?>  
-		</select>
-        <label> Serviço: </label>
-		<select name="id_servico"> 
-		<?php
-		  include("conexao.php");
-		  $query= 'SELECT * FROM servicos ORDER BY servico;';
-		  $resu = mysqli_query($con,$query) or die(mysqli_connect_error());
-		  while ($reg = mysqli_fetch_array($resu)){
-        ?>
-        <option value="<?php echo $reg['id'];?>"> <?php echo $reg['servico'];?> </option> 
-		<?php
-		   
-		  }
-		?>  
-		</select>
-        <p> Observação: <input type="text" size="80" name="observacao" required>
-		<p> <input type="submit" value="Enviar">
-		<p> <input type="reset" value="Limpar">
+        <p> Data de Expiração: <input type="date" name="data_expiracao" required>
+        <p> Valor: <input type="text" size="10" name="valor" required>
+        
+        <div>
+            <label> Prestador: </label>
+            <select required name="id_prestador">
+                <option selected disabled>Selecione um prestador</option>
+                <?php
+                    include("conexao.php");
+                    $query = 'SELECT * FROM prestadores ORDER BY id';
+                    $result = mysqli_query($con, $query) or die(mysqli_connect_error());
+                    while ($cat = mysqli_fetch_array($result)) {
+                ?>
+                <option value="<?php echo $cat['id'] ?>"><?php echo $cat['nome_prest'] ?></option>
+                <?php
+                    }
+                ?>
+            </select>
+        </div>
+        <br>
+        
+        <div>
+            <label> Cliente: </label>
+            <select required name="id_cliente">
+                <option selected disabled>Selecione um cliente</option>
+                <?php
+                    include("conexao.php");
+                    $query = 'SELECT * FROM clientes ORDER BY id';
+                    $result = mysqli_query($con, $query) or die(mysqli_connect_error());
+                    while ($cat = mysqli_fetch_array($result)) {
+                ?>
+                <option value="<?php echo $cat['id'] ?>"><?php echo $cat['nome_cli'] ?></option>
+                <?php
+                    }
+                ?>
+            </select>
+        </div>
+        <br>
+
+        <div>
+            <label> Serviço: </label>
+            <select required name="id_servico">
+                <option selected disabled>Selecione o Serviço</option>
+                <?php
+                    include("conexao.php");
+                    $query = 'SELECT * FROM servicos ORDER BY id';
+                    $result = mysqli_query($con, $query) or die(mysqli_connect_error());
+                    while ($cat = mysqli_fetch_array($result)) {
+                ?>
+                <option value="<?php echo $cat['id'] ?>"><?php echo $cat['servico'] ?></option>
+                <?php
+                    }
+                ?>
+            </select>
+        </div>
+
+        <p> Observação: <input type="text" size="50" name="observacao" required>
+		<p><input type="submit" value="Enviar">   <input type="reset" value="Limpar"> 
 		</form>
 	</body>
 </html>
